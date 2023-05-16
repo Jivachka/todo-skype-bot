272c3e120affa0bc7a448a199e0c908dc15b0c03
Aleks <ya@gmail.com>
2023-05-16 07:20:33 +0300

    Bad code.Need to change

--- class_works.py
+++ class_works.py
@@ -1,13 +1,20 @@
 import logging
+import re
+from datetime import datetime
+
 from skpy import Skype, SkypeFileMsg, SkypeMessageEvent, SkypeTextMsg
 
-from data import allowed_friends
+from data import allowed_friends, login_name, password
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
 
 class SkypeFileManager:
     def __init__(self, username, password):
         self.skype = Skype(username, password)
+        self.message_parse = MessageParser(file_manager)
+        self.reminder_time = ''
+        self.reminder_message = ''
 
     def is_allowed_friend(self, friend_id):
         return friend_id in allowed_friends
@@ -22,14 +29,13 @@ class SkypeFileManager:
             ch = msg.chat
 
             if isinstance(msg, SkypeFileMsg) and self.is_allowed_friend(friend_id):
-                self.send_message(ch, 'Ok.Файл')
-                logging.info(f'{msg.content}')
+                # self.send_message(ch, 'Ok.Файл')
+                # logging.info(f'{msg.content}')
                 return msg.content
             elif isinstance(msg, SkypeTextMsg) and self.is_allowed_friend(friend_id):
-                self.send_message(ch, 'Хорошо написал')
-                logging.info(f'{msg.content}')
+                # self.send_message(ch, 'Хорошо написал')
+                # logging.info(f'{msg.content}')
                 return msg.content
-
         return None
 
     def start_thread(self):
@@ -37,4 +43,24 @@ class SkypeFileManager:
             events = self.skype.getEvents()
             for event in events:
                 if event.type == "NewMessage":
-                    file_content = self.handle_message(event)
+                    self.handle_message(event)
+
+class MessageParser:
+    def __init__(self, skype_manager):
+        self.skype_manager = skype_manager
+        self.pattern = re.compile(r"(\d{2}\.\d{2}\.\d{2}) (\d{2}:\d{2}) (.*)")
+
+    def parse(self, message):
+        match = self.pattern.match(message)
+        if match is None:
+            self.skype_manager.send_message('Invalid message format. Please use the following format: "dd.mm.yy hh:mm Your message"')
+            return None, 'Invalid message format. Please use the following format: "dd.mm.yy hh:mm Your message"'
+
+        date, time, reminder_message = match.groups()
+        reminder_time = datetime.strptime(f'{date} {time}', '%d.%m.%y %H:%M')
+        # logging.info(f'[{reminder_time}]-[{reminder_message}]!!!!!!')
+        return reminder_time, reminder_message
+
+file_manager = SkypeFileManager(login_name, password)
+message_parser = MessageParser(file_manager)
+
--- controllers.py
+++ controllers.py
@@ -1,20 +1,22 @@
+import time
 from threading import Thread
 
-from class_works import SkypeFileManager
+from class_works import SkypeFileManager, MessageParser, file_manager
 from data import login_name, password
 
 
 # Start multiple threads
 def run_main_loop():
     while True:
-        pass
+        time.sleep(1)
 
-file_manager = SkypeFileManager(login_name, password)
+
 
 num_threads = 5
 threads = [Thread(target=file_manager.start_thread) for _ in range(num_threads - 1)]
-threads.append(Thread(target=file_manager.start_thread())) # 1st thread
+threads.append(Thread(target=file_manager.start_thread)) # 1st thread
 threads.append(Thread(target=run_main_loop)) # 2 thread
+
 for t in threads:
     t.start()
 
